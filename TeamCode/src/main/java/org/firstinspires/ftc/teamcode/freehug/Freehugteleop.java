6545c2985ab6260e963a65ac72a48a591554ec99
LAPTOP-PV6H2QFC\fuzzk <fuzzkatty@gmail.com>
2021-02-08 17:21:41 -0900

    pee pee drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/Freehugteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/Freehugteleop.java
@@ -4,18 +4,25 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.Controller;
+import org.firstinspires.ftc.teamcode.marinara.hardware.Intake;
+
 @TeleOp(name = "Free Hugs teleop")
 public class Freehugteleop extends OpMode {
     private Freehugdrive drive;
+    private IntakeFree intake;
+    private Controller controller;
     @Override
     public void init() {
-        drive = new Freehugdrive(this, hardwareMap)
-
+        drive = new Freehugdrive(this, hardwareMap);
+        intake = new IntakeFree(this, hardwareMap);
+        controller = new Controller(gamepad1);
     }
 
     @Override
     public void loop() {
-        drive.drive(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x);
-
+        controller.update();
+        drive.drive(controller.left_stick_x,controller.left_stick_y,controller.right_stick_x);
+        intake.intake(controller.A(), controller.B());
     }
 }
