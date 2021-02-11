a2c279475da965ac74c3387e73647866a2586d23
Tobias Gingerich <>
2021-02-10 18:01:51 -0900

    bug fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/Freehugteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/Freehugteleop.java
@@ -2,10 +2,8 @@ package org.firstinspires.ftc.teamcode.freehug;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Controller;
-import org.firstinspires.ftc.teamcode.marinara.hardware.Intake;
 
 @TeleOp(name = "Free Hugs teleop")
 public class Freehugteleop extends OpMode {
@@ -22,7 +20,7 @@ public class Freehugteleop extends OpMode {
     @Override
     public void loop() {
         controller.update();
-        drive.drive(controller.left_stick_x,controller.left_stick_y,controller.right_stick_x);
-        intake.intake(controller.A(), controller.B());
+        drive.drive(controller.left_stick_x*0.8,-controller.left_stick_y*0.8,controller.right_stick_x);
+        intake.intake(controller.B(), controller.A());
     }
 }
