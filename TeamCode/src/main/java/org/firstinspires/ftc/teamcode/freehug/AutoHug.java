268898155041cf630d88d4b2df8b7a2696cfe221
LAPTOP-PV6H2QFC\fuzzk <fuzzkatty@gmail.com>
2021-02-17 16:48:11 -0900

    automatic hugs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/AutoHug.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.freehug;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+@Autonomous(name="autohug")
+public class AutoHug extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Freehugdrive drive = new Freehugdrive(this, hardwareMap);
+        drive.move(1, 19, 0);
+        drive.turn(1, 9);
+    }
+}
