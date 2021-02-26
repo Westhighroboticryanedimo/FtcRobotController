afb84bced50ccabf28a8509511fa1455ca4e9c93
LAPTOP-PV6H2QFC\fuzzk <fuzzkatty@gmail.com>
2021-02-25 15:01:56 -0900

    updating FreeHugsAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/AutoHug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/AutoHug.java
@@ -6,24 +6,45 @@ import org.firstinspires.ftc.teamcode.marinara.hardware.Webcam;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name="autohug")
 public class AutoHug extends LinearOpMode {
+    GrabberFree grabber;
+
+    public void lowerArmAuto(int ms) {
+        grabber.lowerHand();
+        sleep(ms);
+        grabber.restElbow();
+    }
+    public void raiseArmAuto(int ms) {
+        grabber.handInTheAir();
+        sleep(ms);
+        grabber.restElbow();
+    }
+
+    public void openHandAuto() {
+        grabber.openHand();
+    }
+    public void closeHandAuto() {
+        grabber.closeHand();
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
         Freehugdrive drive = new Freehugdrive(this, hardwareMap);
-        Webcam webcam = new Webcam(this, hardwareMap);
-        GrabberFree grabber = new GrabberFree(this, hardwareMap);
+        WebcamFree webcam = new WebcamFree(this, hardwareMap);
+        grabber = new GrabberFree(this, hardwareMap);
         IntakeFree intake = new IntakeFree(this, hardwareMap);
+
+
 
         ElapsedTime runtime = new ElapsedTime();
 
         waitForStart();
 
         // Get how many rings are stacked
-        int numRingStack = 0;
+        int rings = 0;
         runtime.reset();
         while (opModeIsActive() && runtime.seconds() < 2) {
 
-            numRingStack = webcam.getNumRings();
+            rings = webcam.getNumRings();
 
             telemetry.addData("Num rings", webcam.getNumRings());
             telemetry.addData("Ring position", webcam.getInternalRingNum());
@@ -31,23 +52,23 @@ public class AutoHug extends LinearOpMode {
 
         }
 
-        /*drive.move(1, 20, 0);
-        drive.turn(1, 9);*/
-        int rings;
-        rings = webcam.getNumRings();
+        drive.move(0.6,72,180);
+        sleep(500);
+        drive.turn(1,360);
+        sleep(500);
+        lowerArmAuto(1000);
+        sleep(1000);
+        raiseArmAuto(1000);
 
-        //drive.move(1,72,180);
-       // drive.move(0.6,130,180);
-       // sleep(1000);
-        //drive.move(0.6, 25, -90);
-        //drive.move(0.6,45,0);
-        if (numRingStack == 0) {
+        /*
+        //WEIRD NUMBERS
+        if (rings == 0) {
 
             // 0 ring
             drive.move(0.6,83,180);
             sleep(1000);
             drive.move(0.6, 25, -90);
-        } else if (numRingStack == 1) {
+        } else if (rings == 1) {
 
             // 1 rings
             drive.move(.5, 98, 180);
@@ -61,39 +82,20 @@ public class AutoHug extends LinearOpMode {
             drive.move(0.6, 25, -90);
             drive.move(0.6,45,0);
         }
-
-
-        /*
-        if(rings ==) {
-            drive.move(0.6,130,180);
-            sleep(1000);
-            drive.move(0.6, 30, -90);
-        }
-        else if(rings ==) {
-            drive.move(.6, 98, 180);
-            sleep(1000);
-            drive.move(.6, 16, 0);
-        } else if(rings ==) {
-            drive.move(0.6,83,180);
-            sleep(1000);
-            drive.move(0.6, 25, -90);
-        }
         */
 
-
-
         /*
-        //actual measurements
-        if(rings == ) {
+        //ACTUAL MEASUREMENTS
+        if(rings == 0) {
             drive.move(0.6,74,180);
             sleep(1000);
             drive.move(0.6, 25, -90);
         }
-        else if(rings ==) {
+        else if(rings == 1) {
             drive.move(.6, 92, 180);
             sleep(1000);
-            drive.move(.6, 12, 0);
-        } else if(rings == ) {
+            drive.move(.6, 12, -90);
+        } else {
             drive.move(0.6,112,180);
             sleep(1000);
             drive.move(0.6, 25, -90);
