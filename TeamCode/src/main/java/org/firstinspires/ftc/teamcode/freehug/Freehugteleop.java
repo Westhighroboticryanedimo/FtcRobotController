77e46d42df058eaa01a9ebf28256e2168f8db8cf
LAPTOP-PV6H2QFC\fuzzk <fuzzkatty@gmail.com>
2021-04-09 11:43:23 -0800

    every 5 minutes i remember something that's missing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/Freehugteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/freehug/Freehugteleop.java
@@ -19,6 +19,7 @@ public class Freehugteleop extends OpMode {
     private FreeReturn freeReturn;
     private boolean fullpower;
     private boolean shooting;
+    private o_d_o_m_e_t_r_y odometry;
 
     private GrabberFree grabber;
     double adjustment = 0.07;
@@ -43,13 +44,18 @@ public class Freehugteleop extends OpMode {
         shooterR.setDirection(DcMotor.Direction.REVERSE);
         grabber = new GrabberFree(this, hardwareMap);
         freeReturn = new FreeReturn();
+        odometry = new o_d_o_m_e_t_r_y();
+        odometry.robot_position(hardwareMap.get(DcMotor.class,"odo_vert_L"),hardwareMap.get(DcMotor.class,"odo_vert_R"),hardwareMap.get(DcMotor.class,"odo_horiz"), 307.699557,50);
+        odometry.recalibrate_position();
+        odometry.running = true;
+        odometry.run();
         drive.debug();
 
         freeReturn.xOffset = 0;
         freeReturn.yOffset = 0;
         freeReturn.robotCurrentAngle = 0;
     }
-
+/*
     public void freelyReturn() {
         double xo = freeReturn.xOffset * DISTANCE_CALIBRATION;
         double yo = freeReturn.yOffset * DISTANCE_CALIBRATION;
@@ -79,7 +85,7 @@ public class Freehugteleop extends OpMode {
         sleep(200);
         freeReturn.lockPosition();
         freeReturn.freely_hugging = false;
-    }
+    }*/
 
     public double calculateshooterpowerbasedonbatterypower() {
         return (0.0268 * (drive.getVoltage(hardwareMap)*drive.getVoltage(hardwareMap))) - (0.734 * drive.getVoltage(hardwareMap)) + 5.0128 + SHOOTER_CALIBRATION;
@@ -155,10 +161,28 @@ public class Freehugteleop extends OpMode {
 
         //position lock and return commands
         if(controller.leftBumperOnce()) {
-            freeReturn.lockPosition();
+            odometry.recalibrate_position();
         }
         else if(controller.rightBumperOnce()) {
-            freelyReturn();
+            freeReturn.freely_hugging = true;
+
+            double x_start = odometry.robot_x;
+            double y_start = odometry.robot_y;
+            double x_moved = 0;
+            double y_moved = 0;
+            while(Math.abs(x_moved)<1) {
+                drive.drive(0.40,0,0);
+                x_moved = odometry.give_me_the_X()-x_start;
+            }
+            drive.drive(0,0,0);
+            sleep(20);
+            while(Math.abs(y_moved)<1) {
+                drive.drive(0,0.40,0);
+                y_moved = odometry.give_me_the_Y()-y_start;
+            }
+            drive.drive(0,0,0);
+            sleep(10);
+            freeReturn.freely_hugging = false;
         }
 
 
