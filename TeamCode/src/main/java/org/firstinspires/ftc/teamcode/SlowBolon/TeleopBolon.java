dc81373816fc4a4162a52bb0ac911da4b1a689d9
FuzzKatty <50819754+FuzzKatty@users.noreply.github.com>
2021-09-29 10:39:25 -0800

    ben__sucks
    drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlowBolon/TeleopBolon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlowBolon/TeleopBolon.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.SlowBolon;
 
-public class TeleopBolon {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.Controller;
+import org.firstinspires.ftc.teamcode.freehug.Freehugdrive;
+
+
+@TeleOp(name = "BOLON teleop")
+public class TeleopBolon extends OpMode{
+
+    private DriveBolon drive;
+    private Controller controller;
+
+
+    @Override
+    public void init() {
+        drive = new DriveBolon(this, hardwareMap);
+        controller = new Controller(gamepad1);
+    }
+
+    @Override
+    public void loop() {
+        controller.update();
+        drive.drive(controller.left_stick_x,controller.left_stick_y,controller.right_stick_y);
+    }
 }
