29ad081fbc7932e705bbde010a35ca3d14aa0354
AlpGlide3 <manningzh17@gmail.com>
2021-10-06 14:54:25 -0800

    Copy SlowBolon

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThirdWheel/OdometryBolon.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.SlowBolon;
+
+public class OdometryBolon {
+    public void init() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThirdWheel/TeleopBolon.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.SlowBolon;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.android.AndroidOrientation;
+import org.firstinspires.ftc.teamcode.Controller;
+import org.firstinspires.ftc.teamcode.freehug.Freehugdrive;
+import org.firstinspires.ftc.teamcode.hardware.Gyro;
+
+
+@TeleOp(name = "BOLON teleop")
+public class TeleopBolon extends OpMode{
+
+    private DriveBolon drive;
+    private Controller controller;
+    private Gyro gyro;
+    //private AndroidOrientation orientation;
+
+
+    @Override
+    public void init() {
+        drive = new DriveBolon(this, hardwareMap);
+        gyro = new Gyro(hardwareMap, false);
+        //orientation = new AndroidOrientation();
+        controller = new Controller(gamepad1);
+        drive.togglePOV(true);
+        //orientation.startListening();
+        gyro.reset();
+    }
+
+    @Override
+    public void loop() {
+
+        telemetry.addData("milfs",gyro.getAngleDegrees());
+        /*telemetry.addData("angle",orientation.getAngle());
+        telemetry.addData("angle2", orientation.getAzimuth());
+        telemetry.addData("tobias commited arson", orientatigon-/.etMagnitude());
+        telemetry.addData("tobias executed for arson", orientation.getPitch());
+        telemetry.addData("ben", orientation.getRoll());
+        telemetry.addData("available",orientation.isAvailable());*/
+        telemetry.update();
+        controller.update();
+        drive.drive(controller.left_stick_x,controller.left_stick_y,controller.right_stick_x);
+        drive.togglePOV(controller.leftStickButtonOnce());
+    }
+}
