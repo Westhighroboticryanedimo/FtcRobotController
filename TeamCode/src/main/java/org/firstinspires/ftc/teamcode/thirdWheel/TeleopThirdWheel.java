1d67c4b8069ab580d671286df576e12e6adb9ef5
AlpGlide3 <manningzh17@gmail.com>
2021-10-21 15:43:33 -0800

    Reorganize files: put stuff in hardware directory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thirdWheel/TeleopThirdWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thirdWheel/TeleopThirdWheel.java
@@ -3,24 +3,31 @@ package org.firstinspires.ftc.teamcode.thirdWheel;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.hardware.Gyro;
 import org.firstinspires.ftc.teamcode.Controller;
 
+import org.firstinspires.ftc.teamcode.thirdWheel.hardware;
 
 @TeleOp(name = "ThirdWheel TeleOp")
 public class TeleopThirdWheel extends OpMode {
 
     private DriveThirdWheel drive;
+    private Gyro gyro;
     private Controller controller;
 
     @Override
     public void init() {
         drive = new DriveThirdWheel(this, hardwareMap);
+        gyro = new Gyro(hardwareMap, false);
         controller = new Controller(gamepad1);
         drive.togglePOV(true);
+        gyro.reset();
     }
 
     @Override
     public void loop() {
+        telemetry.addData("gyro", gyro.getAngleDegrees());
+        telemetry.update();
         controller.update();
         drive.drive(controller.left_stick_x,controller.left_stick_y,controller.right_stick_x);
         drive.togglePOV(controller.leftStickButtonOnce());
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thirdWheel/hardware/DriveThirdWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thirdWheel/hardware/DriveThirdWheel.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.thirdWheel;
+package org.firstinspires.ftc.teamcode.thirdWheel.hardware;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
