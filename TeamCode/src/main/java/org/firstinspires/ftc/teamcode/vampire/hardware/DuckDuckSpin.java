f8e82e0346dbd566d0c454b80156c96fdfe269d4
Toya Takahashi <44595617+SKero2004@users.noreply.github.com>
2021-11-07 00:31:02 -0800

    Update Roadrunner and STEM Day
    Refractored roadrunner code. Added drive constants. Updated STEM day shooter code to have variable shoot speed.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vampire/hardware/DuckDuckSpin.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.vampire.hardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.hardware.BaseHardware;
+
+public class DuckDuckSpin extends BaseHardware {
+
+    // Motor and motor power
+    private CRServo duckSpin;
+    private static final double SPIN_POWER = 1;
+
+    // Teleop constructor
+    public DuckDuckSpin(OpMode opMode, HardwareMap hwMap) {
+
+        super(opMode);
+        setup(hwMap);
+
+    }
+
+    // Autonomous constructor
+    public DuckDuckSpin(LinearOpMode opMode, HardwareMap hwMap) {
+
+        super(opMode);
+        setup(hwMap);
+
+    }
+
+    private void setup(HardwareMap hwMap) {
+
+        // Set up servo
+        duckSpin = hwMap.get(CRServo.class, "spin");
+        duckSpin.setDirection(DcMotorSimple.Direction.FORWARD);
+
+    }
+
+    public void spin(boolean button) {
+
+        if (button) duckSpin.setPower(SPIN_POWER);
+        else duckSpin.setPower(0);
+
+    }
+
+}
