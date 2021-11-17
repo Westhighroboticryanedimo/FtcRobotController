10c33e79923966b4a77680f957f1825b23535774
FuzzKatty <50819754+FuzzKatty@users.noreply.github.com>
2021-11-17 14:40:50 -0900

    oo
    oo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slowBolon/AutoBolon.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.SlowBolon;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+@Autonomous(name = "Balloon Mode")
+public class AutoBolon extends LinearOpMode{
+
+    private CamBolon Cam;
+    private int DONDEESTAELDUCKY;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Cam = new CamBolon();
+        Cam.init(hardwareMap);
+        DONDEESTAELDUCKY = Cam.getspot();
+
+        telemetry.addData("where",DONDEESTAELDUCKY);
+        telemetry.addData("leastduckydiff", Cam.pipeline.leastduckydiff);
+        telemetry.addData("leasttapediff", Cam.pipeline.leasttapediff);
+        telemetry.update();
+
+        if(DONDEESTAELDUCKY <= 1) {
+
+        }
+
+        else if(DONDEESTAELDUCKY <= 2) {
+
+        }
+
+        else {
+
+        }
+
+    }
+}
