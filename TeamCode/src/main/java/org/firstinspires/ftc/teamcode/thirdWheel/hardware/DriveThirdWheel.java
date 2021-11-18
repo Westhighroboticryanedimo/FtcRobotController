ae0508106436e0adbd9f85bae79057a302eebc68
AlpGlide3 <manningzh17@gmail.com>
2021-11-17 22:37:32 -0900

    thirdWheel: update PID values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thirdWheel/hardware/DriveThirdWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thirdWheel/hardware/DriveThirdWheel.java
@@ -18,17 +18,17 @@ public class DriveThirdWheel extends HolonomicDrive {
     // Teleop
     public DriveThirdWheel(OpMode opMode, HardwareMap hwMap) {
         super(opMode, hwMap);
+        setup();
     }
 
     // PID setup
     private void setup() {
 
-        setPidDrive(1, 0.01, 0);
+        setPidDrive(0.02, 0, 0.04);
         setPidSpeed(0.05, 0.001, 0);
         setPidTurn(0.028, 0.001, 0);
 
-        setMotorDir(true, true, false, false);
-
+        setMotorDir(false, true, false, true);
         setWheelDiameter(3.7795);
         setTicksPerRev(537.7);
     }
