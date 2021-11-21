f41abcd8d9f69e92209ae10ef31bd0c8780836fa
duckinmyhat <aktobias13@gmail.com>
2021-11-20 15:33:03 -0900

    ooof

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slowBolon/TeleopBolon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slowBolon/TeleopBolon.java
@@ -101,7 +101,7 @@ public class TeleopBolon extends OpMode {
 
         if(controller.B()) {duckDumpy.setPower(1);} else{duckDumpy.setPower(0);}
         telemetry.update();
-        if(controller.XOnce()) {telemetry.speak("We win");}
+        if(controller.XOnce()) {telemetry.speak("We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you");}
         //no changes here you saw nothing
     }
 
