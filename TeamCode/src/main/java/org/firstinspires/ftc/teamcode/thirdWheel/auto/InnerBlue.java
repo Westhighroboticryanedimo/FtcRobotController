4522088afc766e3c06c7396220acca094bff6128
AlpGlide3 <manningzh17@gmail.com>
2021-12-15 10:55:17 -0900

    thirdWheel: init auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/thirdWheel/auto/InnerBlue.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.thirdWheel.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.hardware.Gyro;
+
+import org.firstinspires.ftc.teamcode.thirdWheel.hardware.DriveThirdWheel;
+import org.firstinspires.ftc.teamcode.thirdWheel.hardware.lift.Lift;
+
+@Autonomous(name="ThirdWheel InnerBlue", group="ThirdWheel")
+public class InnerBlue extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DriveThirdWheel drive = new DriveThirdWheel(this, hardwareMap);
+        Lift lift = new Lift(this, hardwareMap);
+
+        // Elapsed time for timed motion
+        ElapsedTime runtime = new ElapsedTime();
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");
+        telemetry.update();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        while (runtime.seconds() < 3) {
+            lift.override(1, -1);
+            lift.assist();
+            drive.setPowers(-0.5, 0.5, 0.5, -0.5);
+        }
+        drive.stop();
+        lift.override(0, -1);
+        lift.assist();
+    }
+
+}
+
+
