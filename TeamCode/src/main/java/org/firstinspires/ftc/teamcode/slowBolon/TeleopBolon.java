b213dc18ed4beb96759266d8a0b6636719cf7bc7
FuzzKatty <50819754+FuzzKatty@users.noreply.github.com>
2022-01-22 09:49:00 -0900

    hhhh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slowBolon/TeleopBolon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slowBolon/TeleopBolon.java
@@ -23,15 +23,19 @@ public class TeleopBolon extends OpMode {
     private DcMotor lift;
 
     private boolean slowbol;
-    private boolean grabtoggle;
     private double speed;
+    private double armposition;
+    private int armupposition = 3000, armdownposition = 0;
+    private int armgoalposition;
 
     private boolean handopen;
 
     @Override
     public void init() {
+        armgoalposition = -1;
+        armupposition = 3000; armdownposition = 0;
         speed=1; handopen = true;
-        slowbol = false; grabtoggle = false;
+        slowbol = false;
         drive = new DriveBolon(this, hardwareMap);
         drive.setup();
         gyro = new Gyro(hardwareMap,false);
@@ -44,6 +48,9 @@ public class TeleopBolon extends OpMode {
         duckDumpy = hardwareMap.get(DcMotor.class,"duckDumpy");
 
         lift = hardwareMap.get(DcMotor.class,"lift");
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         o = new OdometryBolon();
         o.init(hardwareMap.get(DcMotor.class,"frontLeft"));
 
@@ -53,9 +60,30 @@ public class TeleopBolon extends OpMode {
 
     @Override
     public void loop() {
+        armposition = lift.getCurrentPosition();
 
+        if(armgoalposition != -1) {
+            if(armposition > armgoalposition) {lift.setPower(-0.4);}
+            else if(armposition < armgoalposition) {lift.setPower(0.4);}
+
+            if(armposition >= armgoalposition && armgoalposition == armupposition) {armgoalposition = -1;}
+            if(armposition <= armgoalposition && armgoalposition == armdownposition) {armgoalposition = -1;}
+        }
+        if(controller.left_trigger>0.2) {
+            //telemetry.addData("PRESSING","");
+            if(armgoalposition != armupposition && armgoalposition != armdownposition) {
+                telemetry.addData("pressing","");
+                armgoalposition = 3000;
+                telemetry.addData("i am a bad programer","");
+            }
+            else if(armgoalposition == armupposition) {armgoalposition = 0;}
+            else if(armgoalposition == armdownposition) {armgoalposition = -1;}
+        }
+
+        telemetry.addData("lift goal", armgoalposition);
+        telemetry.addData("lift", lift.getCurrentPosition());
         telemetry.addData("milfs", gyro.getAngleDegrees());
-        telemetry.addData("version","32");
+        telemetry.addData("version","33");
 
         controller.update();
         drive.drive(controller.left_stick_x*speed, controller.left_stick_y*speed, controller.right_stick_x);
@@ -64,32 +92,27 @@ public class TeleopBolon extends OpMode {
         if(controller.leftStickButtonOnce()) {slowbol = !slowbol;}
         if(slowbol) {speed=0.4;} else {speed=1;}
 
-        if(controller.dpadLeft()) {
-            //newgrab1.setDirection(CRServo.Direction.FORWARD);newgrab2.setDirection(CRServo.Direction.REVERSE);
-            newgrab1.setPower(1);
-            newgrab2.setPower(-1);
-        }
-        if(controller.rightBumperOnce()) {grabtoggle = !grabtoggle;}
-
-        if(grabtoggle) {
-            //newgrab1.setDirection(CRServo.Direction.REVERSE);newgrab2.setDirection(CRServo.Direction.FORWARD);
+        if(controller.dpadRight()) {
             newgrab2.setPower(1);
             newgrab1.setPower(-1);
+        } else if(controller.dpadLeft()) {
+            newgrab2.setPower(-1);
+            newgrab1.setPower(1);
         } else {
             newgrab2.setPower(0);
             newgrab1.setPower(0);
         }
 
-        if(controller.dpadUp()) {lift.setDirection(DcMotor.Direction.FORWARD);lift.setPower(0.55);}
-        else if(controller.dpadDown()) {lift.setDirection(DcMotor.Direction.REVERSE);lift.setPower(0.4);}
-        else{lift.setPower(0);}
+        if(controller.dpadUp()) {lift.setDirection(DcMotor.Direction.FORWARD);lift.setPower(0.55); armgoalposition = -1;}
+        else if(controller.dpadDown()) {lift.setDirection(DcMotor.Direction.REVERSE);lift.setPower(0.4); armgoalposition = -1;}
+        else if(armgoalposition == -1) {lift.setPower(0);}
 
         if(controller.leftBumperOnce()) {
         }
 
-        if(controller.B()) {duckDumpy.setDirection(DcMotorSimple.Direction.FORWARD);duckDumpy.setPower(1);
+        if(controller.B()) {duckDumpy.setDirection(DcMotorSimple.Direction.FORWARD);duckDumpy.setPower(0.2);
         }
-        else if(controller.Y()) {duckDumpy.setDirection(DcMotorSimple.Direction.REVERSE);duckDumpy.setPower(1);
+        else if(controller.Y()) {duckDumpy.setDirection(DcMotorSimple.Direction.REVERSE);duckDumpy.setPower(0.2);
 }
         else{duckDumpy.setPower(0);}/*newgrab1.setPower(0);newgrab2.setPower(0);}*/
         telemetry.update();
