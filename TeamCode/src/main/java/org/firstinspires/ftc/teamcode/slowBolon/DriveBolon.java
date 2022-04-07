adf43e1e3ecc140fcb5355d7d6f76f70202e7835
AlpGlide3 <manningzh17@gmail.com>
2022-04-06 20:10:23 -0800

    localization: first working version of PointMapLoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slowBolon/DriveBolon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slowBolon/DriveBolon.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.teamcode.hardware.drive.HolonomicDrive;
 public class DriveBolon extends HolonomicDrive {
 
 
-    // For autonomous
+    // For autonomous test
     public DriveBolon(LinearOpMode opMode, HardwareMap hwMap) {
 
         super(opMode, hwMap);
