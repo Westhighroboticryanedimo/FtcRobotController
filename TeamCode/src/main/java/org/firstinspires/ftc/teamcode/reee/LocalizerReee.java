512fac6bb40f1a95630ada9f0a52dada7093e7a4
AlpGlide3 <manningzh17@gmail.com>
2022-04-15 09:06:06 -0800

    reee: pain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/reee/LocalizerReee.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/reee/LocalizerReee.java
@@ -4,16 +4,18 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import com.acmerobotics.roadrunner.drive.Drive;
+
 import org.firstinspires.ftc.teamcode.utils.localization.PointMapLoc;
 
 public class LocalizerReee extends PointMapLoc {
-    public LocalizerReee(LinearOpMode opMode, HardwareMap hwMap) {
-        super(opMode, hwMap);
+    public LocalizerReee(LinearOpMode opMode, HardwareMap hwMap, Drive drive) {
+        super(opMode, hwMap, drive);
         setup();
     }
 
-    public LocalizerReee(OpMode opMode, HardwareMap hwMap) {
-        super(opMode, hwMap);
+    public LocalizerReee(OpMode opMode, HardwareMap hwMap, Drive drive) {
+        super(opMode, hwMap, drive);
         setup();
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/reee/dash.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/reee/dash.java
@@ -1,45 +1,45 @@
-package org.firstinspires.ftc.teamcode.reee;
-
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import org.firstinspires.ftc.teamcode.Controller;
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
-
-import com.acmerobotics.dashboard.FtcDashboard;
-import org.firstinspires.ftc.teamcode.utils.localization.Point;
-
-import org.firstinspires.ftc.teamcode.reee.LocalizerReee;
-
-@TeleOp(name="dash reee", group="reee")
-
-public class dash extends OpMode {
-    LocalizerReee localizer;
-    Controller controller;
-    FtcDashboard dash = FtcDashboard.getInstance();
-
-    @Override
-    public void init() {
-        localizer = new LocalizerReee(this, hardwareMap);
-    }
-
-    @Override
-    public void loop() {
-        if (controller.AOnce()) {
-            localizer.scan();
-            localizer.correctToField();
-        } else if (controller.BOnce()) {
-            localizer.scan();
-            localizer.correctDists();
-            localizer.correctToField();
-        }
-        TelemetryPacket packet = new TelemetryPacket();
-        for (int i = 0; i < localizer.getMap().size(); ++i) {
-            packet.fieldOverlay()
-                    .setFill("blue")
-                    .fillRect(localizer.getMap().get(i).x, localizer.getMap().get(i).y, 2, 2);
-            dash.sendTelemetryPacket(packet);
-        }
-    }
-
-}
+//package org.firstinspires.ftc.teamcode.reee;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import org.firstinspires.ftc.teamcode.Controller;
+//
+//import com.acmerobotics.roadrunner.drive.Drive;
+//import com.acmerobotics.dashboard.FtcDashboard;
+//import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+//
+//import org.firstinspires.ftc.teamcode.reee.LocalizerReee;
+//
+//@TeleOp(name="dash reee", group="reee")
+//
+//public class dash extends OpMode {
+//    LocalizerReee localizer;
+//    Controller controller;
+//    Drive drive;
+//    FtcDashboard dash = FtcDashboard.getInstance();
+//
+//    @Override
+//    public void init() {
+//        localizer = new LocalizerReee(this, hardwareMap, drive);
+//    }
+//
+//    @Override
+//    public void loop() {
+//        if (controller.AOnce()) {
+//            localizer.scan();
+//            localizer.correctToField();
+//        } else if (controller.BOnce()) {
+//            localizer.scan();
+//            localizer.correctDists();
+//            localizer.correctToField();
+//        }
+//        TelemetryPacket packet = new TelemetryPacket();
+//        for (int i = 0; i < localizer.getMap().size(); ++i) {
+//            packet.fieldOverlay()
+//                    .setFill("blue")
+//                    .fillRect(localizer.getMap().get(i).x, localizer.getMap().get(i).y, 2, 2);
+//            dash.sendTelemetryPacket(packet);
+//        }
+//    }
+//
+//}
