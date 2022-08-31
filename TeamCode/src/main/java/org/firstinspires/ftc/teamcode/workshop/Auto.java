eddcfcbfa26a85f1e631b6b1f6db30964f08512a
AlpGlide3 <manningzh17@gmail.com>
2022-08-31 10:49:17 -0800

    Template auto and teleop for workshop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/workshop/Auto.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.workshop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name="Amogus Auto")
+public class Auto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/workshop/Teleop.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.workshop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Controller;
+
+@TeleOp(name = "Put a unique name here!")
+public class Teleop extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
