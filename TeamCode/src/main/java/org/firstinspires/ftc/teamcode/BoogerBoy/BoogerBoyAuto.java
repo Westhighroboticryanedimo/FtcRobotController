8466bccdbd3912e29dd23f6f4309d59149857997
B100GH <B100@b100.dev>
2022-10-10 15:21:17 -0800

    Auto For Westy Beast

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoogerBoy/BoogerBoyAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoogerBoy/BoogerBoyAuto.java
@@ -11,13 +11,48 @@ import org.firstinspires.ftc.teamcode.slowBolon.DriveBolon;
 @Autonomous(name = "minicat")
 public class BoogerBoyAuto extends LinearOpMode{
     private BoogerBoyDrive drive;
+    private int AI_resualt;
+    // This is for when the cone is in the first case
+    private void uno()
+    {
+        drive.move(1,60,0);
+    }
+    // This is for when the cone is in the seconds case
+    private void dos()
+    {
+        drive.move(1,60,0);
+        drive.move(1,60,90);
+    }
+    // This is for when the cone is in the third case
+    private void tres()
+    {
+        drive.move(1,60,0);
+        drive.move(1,60,-90);
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
         drive = new BoogerBoyDrive(this,hardwareMap);
         ElapsedTime runtime = new ElapsedTime();
-        drive.move(10,10,10);
+
+        // AI Goes Here and it makes a decision between the options for thr cone
+        // Some vision recognition detects one of three options and resualts 1,2,3
+        AI_resualt = 1;
+
+        // Theese detect what the AI Detected and run the corasponding code
+        if(AI_resualt == 1)
+        {
+            uno();
+        }
+        if(AI_resualt == 2)
+        {
+            dos();
+        }
+        if(AI_resualt == 3)
+        {
+            tres();
+        }
 
 
 
