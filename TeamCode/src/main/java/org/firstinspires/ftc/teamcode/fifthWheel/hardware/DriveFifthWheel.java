e71ebc49618a4f53539d474a717da35c7c26241a
consla <conslaaa@proton.me>
2022-10-21 12:42:31 -0800

    what was this for again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/hardware/DriveFifthWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/hardware/DriveFifthWheel.java
@@ -27,9 +27,9 @@ public class DriveFifthWheel extends HolonomicDrive {
         isDrivePOV = false;
 
 
-        setPidDrive(0.035, 0, 0.01);
+        setPidDrive(0.05, 0, 0.01);
         setPidSpeed(0.05, 0.001, 0);
-        setPidTurn(0.028, 0.001, 0);
+        setPidTurn(0.05 , 0.001, 0);
 
         setMotorDir(false, true, false, true);
         setWheelDiameter(3.7795);
