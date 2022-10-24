01def9f55033a4edfe2c820baff0c97c6f22c095
consla <conslaaa@proton.me>
2022-10-24 09:04:49 -0800

    fw(intake): add opposite servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/hardware/intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/hardware/intake/Intake.java
@@ -1,15 +1,12 @@
 package org.firstinspires.ftc.teamcode.fifthWheel.hardware.intake;
 
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.teamcode.utils.control.DcMotorUtils;
 
 public class Intake {
     private Servo grip;
     private Servo flipLeft;
-    // private Servo flipRight;
+    private Servo flipRight;
 
     private double gripPos = 1;
     private double flipPos = 0;
@@ -21,33 +18,36 @@ public class Intake {
     private void init(HardwareMap hwMap) {
         grip = hwMap.get(Servo.class, "grip");
         flipLeft = hwMap.get(Servo.class, "flipLeft");
-        // flipRight = hwMap.get(Servo.class, "flipRight");
+        flipRight = hwMap.get(Servo.class, "flipRight");
 
-        flipLeft.scaleRange(0, 1);
-        // flipRight.setDirection(Servo.Direction.REVERSE);
+        // flipLeft.scaleRange(0, 1);
+        flipRight.setDirection(Servo.Direction.REVERSE);
         // flipRight.scaleRange(0, 1);
     }
 
     public void lower() {
         flipPos = 0.5;
         flipLeft.setPosition(flipPos);
+        flipRight.setPosition(flipPos);
     //     flipRight.setPosition(1);
     }
 
     public void raise() {
         flipPos = 1;
         flipLeft.setPosition(flipPos);
-        // flipRight.setPosition(0);
+        flipRight.setPosition(flipPos);
     }
 
     public void flipIncrease() {
         flipPos += 0.05;
         flipLeft.setPosition(flipPos);
+        flipRight.setPosition(flipPos);
     }
 
     public void flipDecrease() {
         flipPos -= 0.05;
         flipLeft.setPosition(flipPos);
+        flipRight.setPosition(flipPos);
     }
 
     public double flipPos() {
