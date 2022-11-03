08c6537466273b8527c52da83c70538b3259cfd1
consla <conslaaa@proton.me>
2022-11-03 12:52:16 -0800

    fw:refactor(DRCB): remove useless functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/subsystem/DRCB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/subsystem/DRCB.java
@@ -12,7 +12,6 @@ import org.firstinspires.ftc.teamcode.PIDController;
 public class DRCB {
     public DcMotor leftMotor;
     public DcMotor rightMotor;
-    // private DcMotor rightMotor;
     private int level = 0;
     private int ticks = 0;
 
@@ -47,57 +46,8 @@ public class DRCB {
         rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
-    public void setLevel(int l) {
-        new Thread() {
-            @Override
-            public void run() {
-//                pidArm.enable();
-//                pidArm.setSetpoint(LEVELS[l]);
-//                pidArm.setTolerance(5);
-//                double power = 0;
-//                while (!pidArm.onTarget()) {
-//                    power = pidArm.performPID(rightMotor.getCurrentPosition());
-//                    if (power < 0) {
-//                        power = power / 10;
-//                    }
-//                    leftMotor.setPower(power);
-//                    rightMotor.setPower(power);
-//                }
-//                leftMotor.setPower(0.16);
-//                rightMotor.setPower(0.16);
-                while (DcMotorUtils.arrived(rightMotor, LEVELS[l], 10)) {
-                    DcMotorUtils.moveByTicks(leftMotor, LEVELS[l], 50, 10);
-                    DcMotorUtils.moveByTicks(rightMotor, LEVELS[l], 50, 10);
-                }
-                leftMotor.setPower(0.16);
-                rightMotor.setPower(0.16);
-            }
-        }.start();
     }
 
-    // public void increaseHeight() {
-    //     new Thread() {
-    //         @Override
-    //         public void run() {
-    //             ticks = DcMotorUtils.getCurrentTicks(leftMotor);
-    //             while (!DcMotorUtils.arrived(leftMotor, ticks + 500, 100)) {
-    //                 DcMotorUtils.moveByTicks(leftMotor, ticks + 500, 100, 100);
-    //             }
-    //         }
-    //     }.start();
-    // }
-
-    // public void decreaseHeight() {
-    //     new Thread() {
-    //         @Override
-    //         public void run() {
-    //             ticks = DcMotorUtils.getCurrentTicks(leftMotor);
-    //             while (!DcMotorUtils.arrived(leftMotor, ticks - 500, 100)) {
-    //                 DcMotorUtils.moveByTicks(leftMotor, ticks - 500, 100, 100);
-    //             }
-    //         }
-    //     }.start();
-    // }
 
     public int getCurrentLeftTicks() {
         return leftMotor.getCurrentPosition();
