aaab0057ff3bc333ecb6ba0388c89c978fe23826
consla <conslaaa@proton.me>
2022-11-03 12:52:39 -0800

    fw:feat(DRCB): add torque feedforward calculation
    Return arbitrary torque units given angle of motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/subsystem/DRCB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fifthWheel/subsystem/DRCB.java
@@ -19,6 +19,14 @@ public class DRCB {
     private static final double LIFT_POWER = 0.57;
     private static final double LOWER_POWER = -0.05;
 
+    private static final double TICKS_PER_REV = 1120;
+    private static final double L_0 = 3; // motor to pivot dist
+    private static final double L_A = 2; // bottom linkage
+    private static final double L_B = 3.75; // top linkage
+    private static final double L_OFFSET = 2.4; // linkage attachment dist
+    private static final double THETA_0 = 114; // angle between horizontal and L_0
+    private static final double kP_ff = 0.01; // gain for torque feedforward
+
     private PIDController pidArm = new PIDController(1, 0, 0);
 
     public DRCB(HardwareMap hwMap) {
@@ -46,8 +54,23 @@ public class DRCB {
         rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    public double calculateFeedforward(double angle) {
+        double l_1 = Math.sqrt(Math.pow(L_A, 2)
+                               + Math.pow(L_0, 2)
+                               - 2*L_A*L_0*Math.cos(THETA_0 - angle));
+        double theta_ab = lawOfCos(L_0, l_1, L_A) + lawOfCos(L_OFFSET, l_1, L_B);
+        double theta_bc = lawOfCos(l_1, L_B, L_OFFSET);
+        double theta = 180 - (theta_ab - angle) - theta_bc;
+
+        return kP_ff*Math.cos(theta)/(Math.sin(theta_ab)*Math.sin(theta_bc));
     }
 
+    // calculate angle of an SSS triangle
+    // @param a is the side opposite to the angle you want to find
+    private double lawOfCos(double a, double b, double c) {
+        return Math.acos((Math.pow(b, 2) + Math.pow(c, 2) - Math.pow(a, 2))
+                         / (2*b*c));
+    }
 
     public int getCurrentLeftTicks() {
         return leftMotor.getCurrentPosition();
