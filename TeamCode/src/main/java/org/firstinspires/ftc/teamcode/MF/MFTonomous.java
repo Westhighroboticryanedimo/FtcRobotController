d63bf4acb0fe75240786b7fc2a55d8202cfce17a
iovodeverett <118243441+iovodeverett@users.noreply.github.com>
2022-12-17 10:45:12 +0300

    Update MFTonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MF/MFTonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MF/MFTonomous.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.MF;
 
+import static java.lang.Math.abs;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import static java.lang.Math.abs;
 
 @Autonomous(name = "Read/Park")
 
@@ -14,8 +17,37 @@ public class MFTonomous extends LinearOpMode {
     private DcMotor BLDrive;
     private DcMotor BRDrive;
 
+    private class MotorFns {
+
+        private int getEncoders() {
+            return((abs(FRDrive.getCurrentPosition())+abs(BLDrive.getCurrentPosition()))/2);
+        }
+        private void resetEncoders() {
+            FRDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            FRDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            BLDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            BLDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        private void stopMotors() {
+            FLDrive.setPower(0);
+            FRDrive.setPower(0);
+            BLDrive.setPower(0);
+            BRDrive.setPower(0);
+        }
+        private void runMotors(double FL, double BL, double FR, double BR) {
+            FLDrive.setPower(FL);
+            BLDrive.setPower(BL);
+            FRDrive.setPower(FR);
+            BRDrive.setPower(BR);
+        }
+
+    }
+
+    private MotorFns motorFns;
+
     @Override public void runOpMode() throws InterruptedException {
         colorCam.cameraInit(hardwareMap);
+        motorFns = new MotorFns();
         FLDrive = hardwareMap.get(DcMotor.class, "frontLeft");
         FRDrive = hardwareMap.get(DcMotor.class, "frontRight");
         BLDrive = hardwareMap.get(DcMotor.class, "backLeft");
@@ -25,40 +57,20 @@ public class MFTonomous extends LinearOpMode {
         telemetry.addData("FREncoder", FRDrive.getCurrentPosition());
         telemetry.addData("BLEncoder", BLDrive.getCurrentPosition());
         telemetry.update();
-        FRDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FRDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BLDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BLDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        motorFns.resetEncoders();
 
         waitForStart();
-        while (FRDrive.getCurrentPosition() > -2000 && BLDrive.getCurrentPosition() < 2000) {
-            FLDrive.setPower(0.5);
-            FRDrive.setPower(-0.5);
-            BLDrive.setPower(0.5);
-            BRDrive.setPower(-0.5);
+        while (motorFns.getEncoders() < 2000) {
+            motorFns.runMotors(0.5, 0.5, -0.5, -0.5);
             telemetry.update();
         }
-        FLDrive.setPower(0);
-        FRDrive.setPower(0);
-        BLDrive.setPower(0);
-        BRDrive.setPower(0);
-        FRDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        FRDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BLDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        BLDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFns.stopMotors();
+        motorFns.resetEncoders();
         while (FRDrive.getCurrentPosition() < 2000 && BLDrive.getCurrentPosition() > -2000) {
-            FLDrive.setPower(-0.5);
-            FRDrive.setPower(0.5);
-            BLDrive.setPower(-0.5);
-            BRDrive.setPower(0.5);
+            motorFns.runMotors(-0.5, -0.5, 0.5, 0.5);
             telemetry.update();
         }
-        FLDrive.setPower(0);
-        FRDrive.setPower(0);
-        BLDrive.setPower(0);
-        BRDrive.setPower(0);
-        colorCam.getColor();
+        motorFns.stopMotors();
 
     }
 }
