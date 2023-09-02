3a758de2ab6133610f3061fb8df8d045f6f02803
consla <conslaaa@proton.me>
2023-09-01 20:48:12 -0800

    Switch gyro to use new generic IMU interface

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Gyro.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class Gyro {
 
-    private BNO055IMU imu;
+    private IMU imu;
 
     private double globalAngle;
     private Orientation lastAngles;
@@ -18,18 +19,20 @@ public class Gyro {
     public Gyro(HardwareMap hwMap) {
 
         // IMU parameters
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.mode = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.loggingEnabled = false;
+//        IMU.Parameters = new IMU.Parameters();
+
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        parameters.mode = BNO055IMU.SensorMode.IMU;
+//        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+//        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+//        parameters.loggingEnabled = false;
 
         // Define and initialize IMU
-        imu = hwMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
+        imu = hwMap.get(IMU.class, "imu");
+//        imu.initialize(parameters);
 
         // Calibrate gyro
-        while (!imu.isGyroCalibrated()) {}
+//        while (!imu.) {}
 
         reset();
 
@@ -38,18 +41,18 @@ public class Gyro {
     public Gyro(HardwareMap hwMap, boolean isCalibrate) {
 
         // IMU parameters
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.mode = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.loggingEnabled = false;
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        parameters.mode = BNO055IMU.SensorMode.IMU;
+//        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+//        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+//        parameters.loggingEnabled = false;
 
         // Define and initialize IMU
-        imu = hwMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
+        imu = hwMap.get(IMU.class, "imu");
+//        imu.initialize(parameters);
 
         // If isCalibrate, calibrate gyro
-        if (isCalibrate) while (!imu.isGyroCalibrated()) {}
+//        if (isCalibrate) while (!imu.isGyroCalibrated()) {}
 
         reset();
 
@@ -57,14 +60,15 @@ public class Gyro {
 
     public void reset() {
 
-        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        lastAngles = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+//        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         globalAngle = 0;
 
     }
 
     public double getAngleDegrees() {
 
-        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        Orientation angles = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
         double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
 
